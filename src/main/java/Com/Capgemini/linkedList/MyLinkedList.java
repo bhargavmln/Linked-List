df4656ddc3143da7ed3c9156c5e31444b106e6de
bhargavmln <bhargavmln@gmail.com>
2020-10-14 22:13:05 +0530

    [bhargavmln]add:added pop method

--- src/main/java/Com/Capgemini/linkedList/MyLinkedList.java
+++ src/main/java/Com/Capgemini/linkedList/MyLinkedList.java
@@ -39,6 +39,12 @@ public class MyLinkedList {
 		myNode.setNext(newNode);
 		newNode.setNext(tempNode);
 	}
+	
+	public INode pop() {
+		INode tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;
+	}
 
 	public void printMyNodes() {
 		StringBuffer myNodes = new StringBuffer("My Nodes: ");
