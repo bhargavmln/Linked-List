2560cadb9af53e29cb452df5868c59618f9dd7ff
bhargavmln <bhargavmln@gmail.com>
2020-10-15 20:48:02 +0530

    [bhargavmln]add:ability to pop the last added node

--- src/test/java/Com/Capgemini/linkedList/MyStackTest.java
+++ src/test/java/Com/Capgemini/linkedList/MyStackTest.java
@@ -19,4 +19,17 @@ public class MyStackTest {
 		Assert.assertEquals(myThirdNode,peak);
 	}
 	
+	@Test
+	public void given3NumbersInStack_WhenPoped_ShouldMatchLastAddedNode() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyStack myStack = new MyStack();
+		myStack.push(myFirstNode);
+		myStack.push(mySecondNode);
+		myStack.push(myThirdNode);
+		INode pop = myStack.pop();
+		myStack.printStack();
+		Assert.assertEquals(myThirdNode,pop);
+	}
 }
